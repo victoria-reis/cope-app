47bc3f34e7db9f738705bd090ae4caf2c577b445
Victoria <victoriareis_@outlook.com>
2022-06-28 14:04:22 -0400

    added a model to confirm user wants to leave current session

--- src/screens/NewSessionScreen.js
+++ src/screens/NewSessionScreen.js
@@ -1,13 +1,22 @@
 // modules
 import React from "react";
-import { SafeAreaView, View, Text, Platform, StatusBar } from "react-native";
+import {
+	SafeAreaView,
+	TouchableOpacity,
+	Text,
+	Platform,
+	StatusBar,
+	View,
+} from "react-native";
 import { useState, useEffect } from "react";
 import styled from "styled-components";
+import { AntDesign } from "@expo/vector-icons";
 
 // components
 import AnxietyRating from "../components/AnxietyRating";
 import AnxietyCategories from "../components/AnxietyCategories";
 import VoiceRecording from "../components/VoiceRecording";
+import CloseModal from "../components/CloseModal";
 
 // new session screen
 const NewSessionScreen = ({ navigation }) => {
@@ -15,11 +24,29 @@ const NewSessionScreen = ({ navigation }) => {
 	const [feeling, setFeeling] = useState("");
 	const [showCategories, setShowCategories] = useState(false);
 	const [category, setCategory] = useState("");
+	const [modalVisible, setModalVisible] = useState(false);
+
+	const handleClose = () => {
+		setModalVisible(true);
+	};
 
 	return (
 		<ScreenContainer
 			style={Platform.OS ? { marginTop: StatusBar.currentHeight } : null}
 		>
+			<CloseButton onPress={handleClose}>
+				<Text>
+					<AntDesign name="close" size={30} color="black" />
+				</Text>
+			</CloseButton>
+
+			<CloseModal
+				modalVisible={modalVisible}
+				setModalVisible={setModalVisible}
+				navigation={navigation}
+				setShowRating={setShowRating}
+			/>
+
 			{showRating ? (
 				//where user will rate their anxiety
 				<AnxietyRating
@@ -36,7 +63,7 @@ const NewSessionScreen = ({ navigation }) => {
 					setShowCategories={setShowCategories}
 				/>
 			) : !showRating && !showCategories ? (
-				// where user will choose if they want to record voice or type
+				// where user will record voice
 				<VoiceRecording />
 			) : null}
 		</ScreenContainer>
@@ -47,6 +74,11 @@ const NewSessionScreen = ({ navigation }) => {
 const ScreenContainer = styled(SafeAreaView)`
 	flex: 1;
 	padding: 20px;
+`;
+
+const CloseButton = styled(TouchableOpacity)`
+	position: absolute;
+	right: 20px;
 `;
 
 export default NewSessionScreen;
